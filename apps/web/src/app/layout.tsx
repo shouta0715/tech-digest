d4ff45d3bb1e0fae3247b107f6012771425cdbb1
shouta0715 <kshouta0715@gmail.com>
2025-03-16 20:52:31 +0900

    add: gesit font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,11 +1,16 @@
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import '../styles/global.css';
+import { cn } from '@/utils/cn';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
-      <body>{children}</body>
+    <html lang="ja" className={cn(GeistSans.variable, GeistMono.variable)}>
+      <body className="font-geist-sans text-gray-1000">{children}</body>
     </html>
   );
 }
