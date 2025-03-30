cff11da8f6961876470dc6767903606dee940422
shouta0715 <kshouta0715@gmail.com>
2025-03-30 20:35:49 +0900

    WIP

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import turboPlugin from 'eslint-plugin-turbo';
-import tseslint from 'typescript-eslint';
-import onlyWarn from 'eslint-plugin-only-warn';
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+import unusedImports from "eslint-plugin-unused-imports";
+import importPlugin from "eslint-plugin-import";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -11,22 +12,52 @@ import onlyWarn from 'eslint-plugin-only-warn';
  * */
 export const config = [
   js.configs.recommended,
-  eslintConfigPrettier,
   ...tseslint.configs.recommended,
   {
+    name: "plugins",
     plugins: {
       turbo: turboPlugin,
+      "unused-imports": unusedImports,
+      import: importPlugin,
+      prettier: eslintConfigPrettier,
     },
+  },
+  {
+    name: "ignores",
+    ignores: ["dist/**", "node_modules/**", ".next/**"],
+  },
+  {
+    name: "global-typescript-rules",
     rules: {
-      'turbo/no-undeclared-env-vars': 'warn',
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          js: "never",
+          ts: "never",
+        },
+      ],
+      "import/order": [
+        "error",
+        {
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
-  },
-  {
-    plugins: {
-      onlyWarn,
-    },
-  },
-  {
-    ignores: ['dist/**'],
   },
 ];
